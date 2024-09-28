27de83c9deb7232f2422b73ac04b023fa8f7cc52
Afnan Sayed <afnansayed1973@gimal.com>
2024-09-28 21:38:28 +0600

    banner with navbar

---
+++ src/components/HomePage/BannerWithNavbar/Banner.jsx
@@ -0,0 +1,100 @@
+"use client";
+import Nabvar from "@/components/sheared/navbar/Nabvar";
+import React from "react";
+import { GiSundial } from "react-icons/gi";
+import { TypeAnimation } from "react-type-animation";
+
+const Banner = () => {
+  const navLink = (
+    <>
+      <li className="mr-6 text-xl font-semibold">Our Services</li>
+      <li className="mr-6 text-xl font-semibold"> Package</li>
+      <li className="mr-6 text-xl font-semibold">About</li>
+    </>
+  );
+  return (
+    <div className="bg-gradient-to-r from-sky-300 via-yellow-300 via-green-400 to-cyan-200 p-6">
+      {/* Navbar Start */}
+      <div className="w-[90%] mx-auto ">
+        <div className="navbar">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              >
+                {navLink}
+              </ul>
+            </div>
+            <div className="flex items-center justify-center gap-3">
+              <span className="text-6xl mb-3 text-sky-700">
+                <GiSundial />
+              </span>{" "}
+              <h1 className="text-6xl font-extrabold font-serif text-sky-700">
+                ZSI
+              </h1>
+            </div>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">{navLink}</ul>
+          </div>
+          <div className="navbar-end">
+            <button className="w-24 rounded-md py-2 border-2 border-black mr-4 shadow-lg shadow-[#fff] hover:scale-105 transform transition-transform duration-300 ease-in-out font-semibold">
+              Register
+            </button>
+            <button className="w-24 rounded-md py-2 border-2 border-black shadow-lg shadow-[#fff] hover:scale-105 transform transition-transform duration-300 ease-in-out font-semibold">
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* Navbar end */}
+
+      {/* Banner Content*/}
+      <div className="w-[90%] mx-auto mt-6">
+        <h1 className="text-8xl font-bold uppercase text-gray-800">
+          <span className="block mb-5">we help your</span>
+          <span className="block mb-5">business grow</span>
+          <span className="block mb-5">
+            with{" "}
+            <span className="bg-gradient-to-t from-sky-900 to-cyan-500 bg-clip-text text-transparent">
+              <TypeAnimation
+                style={{
+                  wordBreak: "break-all",
+                }}
+                sequence={["digital", 1000, "", 1000]}
+                wrapper="span"
+                speed={20}
+                repeat={Infinity}
+                cursor={true}
+              />
+            </span>
+          </span>
+          <span className="block">marketing</span>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
