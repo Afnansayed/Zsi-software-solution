2d0d9d20ee030f42fc447f8da6702201ac9b7e63
Afnan Sayed <afnansayed1973@gimal.com>
2024-09-30 02:56:30 +0600

    cart motion

--- src/components/HomePage/Services/Service.jsx
+++ src/components/HomePage/Services/Service.jsx
@@ -1,20 +1,52 @@
+'use client'
 import Image from "next/image";
 import React from "react";
+import {motion, useAnimation} from 'framer-motion';
+import { useInView } from "react-intersection-observer";
 
 const Service = ({ service }) => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.2 }); 
+
+  React.useEffect(() => {
+      console.log('In View:', inView);
+      if (inView) {
+        controls.start("visible");
+      }
+    }, [controls, inView]);
+  
+    const boxVariants = {
+      hidden: { opacity: 0, x: 200 }, 
+      visible: { opacity: 1, x: 0, transition: { duration: 1 } }, 
+    }; 
   return (
-    <div className="card glass w-72  mt-12 md:mt-16 relative">
+    <div className="card glass w-full md:w-72  mt-12 md:mt-16 relative">
       <div className="card-body backdrop-blur-lg bg-white/10 rounded-xl ">
-      <figure>
-         <Image className="w-[220px] h-[150px]" src={service?.service_image} alt="service image" width={200} height={200}></Image>
-      </figure>
+      <motion.figure
+      ref={ref}
+      initial="hidden"
+      animate={controls}
+      variants={boxVariants}
+      >
+         <Image
+          className="w-[220px] h-[150px]" 
+          src={service?.service_image} alt="service image" width={200} height={200}></Image>
+      </motion.figure>
         <h2 className="text-md text-gray-800 font-semibold">{service?.service_name}</h2>
           <div className="h-32 ">
             <p className="mt-3">{service?.service_details}</p>
           </div>
-        <div className="absolute -top-10 right-1">
+        <motion.div
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={{
+          hidden: { opacity: 0, x: -200 }, 
+          visible: { opacity: 1, x: 0, transition: { duration: 3 }, delay: 2 }, 
+        }}
+        className="absolute -top-10 right-1">
           <button className="w-24 rounded-md py-2 border-2 border-black shadow-lg shadow-[#fff] hover:scale-105 transform transition-transform duration-300 ease-in-out font-semibold ">Learn now!</button>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
